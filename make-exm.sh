af0781fe1f698234bd809c3c57757e8f85ae8b8a
samsesh <admin@samsesh.net>
2022-04-12 14:16:53 +0430

    update

--- make-exm.sh
+++ make-exm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+eval $(cat .env | sed 's/^/export /')
 docker run -it --rm \
     --mount type=volume,src=synapse-data,dst=/data \
     -e SYNAPSE_SERVER_NAME=${HOSTNAME} \
@@ -8,3 +9,6 @@ docker run -it --rm \
 mkdir synapse-data
 mv  /var/lib/docker/volumes/synapse-data/_data/ synapse-data
 docker volume rm synapse-data/
+mkdir element-web-config
+cd element-web-config
+wget https://raw.githubusercontent.com/dustinbrun/docker-compose-homeserver/main/matrix-extensions/element-web-config/config.json
